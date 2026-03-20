98d6bc42e5d574c5a4a3c0389b338b513c8755ef
definitcode <155143855+definitcode@users.noreply.github.com>
2026-03-20 15:16:10 -0700

    Patch #2

---
+++ check_ops.ts
@@ -0,0 +1,6 @@
+import { ScriptOpcode } from './engine/src/engine/script/ScriptOpcode.js';
+console.log('STAT_BASE:', ScriptOpcode.STAT_BASE); // 180 in Step 2830
+console.log('STAT_LEVEL:', ScriptOpcode.STAT); // 187 in Step 2830
+console.log('P_COUNTDIALOG:', ScriptOpcode.P_COUNTDIALOG);
+console.log('P_OPHELD:', ScriptOpcode.P_OPHELD);
+console.log('CLAN_NAME:', ScriptOpcode.CLAN_NAME);
